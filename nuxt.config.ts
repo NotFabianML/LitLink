f51e5ae5134593085a97fc6a1e43b5eb67a9e11a
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2025-04-23 18:23:46 -0600

    build: minor changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,20 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL
+    }
+  },
+  modules: [
+    '@pinia/nuxt'
+  ],
+  plugins: [
+    '~/plugins/axios.ts',
+    '~/plugins/auth.ts',
+    '~/plugins/preferences.ts',
+    '~/plugins/motion.ts'
+  ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   experimental: {
